15cea506db797c540f628a9bfbe8830eb61f21de
snehag1111 <sneha_gupta@epam.com>
2025-06-07 11:27:09 +0530

    SearchForm

---
+++ src/SearchBox.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+
+function SearchBox(props) {
+    const[query, setQuery] = useState(props.initialQuery);
+
+    const handleQueryChange = (event) => {
+        setQuery(event.target.value);
+    };
+
+    const handleSearch = () => {
+        props.onSearch(query);
+    }
+
+    const handlePressEnter = (event) => {
+        if(event.key === 'Enter') {
+            handleSearch();
+        }
+    }
+
+    const inputStyle = {
+        backgroundColor: '#333',
+        color: '#c0c0c0',
+        border: '1px solid #666',
+        padding: '0 16px',
+        width: '713px',
+        height: '57px',
+        fontSize: '16px'
+    }
+
+    const buttonStyle = {
+        backgroundColor: '#e74c3c',
+        color: '#fff',
+        border: 'none',
+        padding: '16px 24px',
+        borderRadius: '4px',
+        cursor: 'pointer',
+        fontWeight: 'bold',
+        fontSize: '16px',
+        transition: 'background-color 0.3s',
+        height: '57px',
+        width: '233px',
+    }
+
+    const containerStyle = {
+        position: 'absolute',
+        top: '242px',
+        left: '178px',
+        display: 'flex',
+        gap: '12px'
+    }
+
+    const handleButtonHover = (event) => {
+        event.target.style.backgroundColor = '#c0392b';
+    }
+
+    const handleButtonOut = (event) => {
+        event.target.style.backgroundColor = '#e74c3c'
+    }
+
+    return React.createElement(
+        'div',
+        { style: containerStyle },
+        React.createElement(
+            'input',
+            { 
+                type: 'text', 
+                value: query, 
+                onChange: handleQueryChange, 
+                onKeyPress: handlePressEnter, 
+                placeholder: 'What do you want to watch?', 
+                style: inputStyle 
+            }
+        ),
+        React.createElement(
+            'button',
+            { 
+                onClick: handleSearch,
+                onMouseEnter: handleButtonHover,
+                onMouseLeave: handleButtonOut,
+                style: buttonStyle
+             },
+            'Search'
+        )
+    );
+}
+
+export default SearchBox;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Counter from './Counter';
+import SearchBox from './SearchBox';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   React.createElement(
-    Counter,
-    { initialValue: 0 }
+  //   Counter,
+  //   { initialValue: 0 }
+  // ),
+  // React.createElement(
+    SearchBox, 
+    { initialQuery: 'Baby\'s Day Out',
+      onSearch: handleSearch
+     }
   )
 );
+
+function handleSearch(query) {
+  alert(`Searching for: ${query}`);
+}
